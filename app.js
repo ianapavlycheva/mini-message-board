15e9473f779b9b4b5e0de7f54eb1d5347918cb01
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2025-04-22 16:42:56 +1000

    Refactor routes

--- app.js
+++ app.js
@@ -2,37 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+const indexRouter = require("./routes/index");
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
-app.get("/", (req, res) => {
-  res.render("index", { messages: messages });
-});
-
-app.get("/new", (req, res) => {
-  res.render("form");
-});
-
-app.post("/new", (req, res) => {
-  const { user, text } = req.body;
-  messages.push({ user, text, added: new Date() });
-  res.redirect("/");
-});
+app.use("/", indexRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+router.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+});
+
+router.get("/new", (req, res) => {
+  res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  const { user, text } = req.body;
+  messages.push({ user, text, added: new Date() });
+  res.redirect("/");
+});
+
+module.exports = router;
