e02005b236c2b0151bb5a46ccc8e1738913e5af9
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2025-04-22 17:12:55 +1000

    Add style

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.use(express.static("public"));
+
 const indexRouter = require("./routes/index");
 app.use(express.urlencoded({ extended: false }));
 
